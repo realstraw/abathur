2674b09985aaf2790f9926882cd91c24fa776166
Kexin Xie <ikexinxie@gmail.com>
2014-03-07 18:32:16 +1100

    main framework for cluster and tests, need to fix the kmeans test cases

---
+++ tests/_abathur/test_cluster.py
@@ -0,0 +1,65 @@
+import unittest
+import numpy as np
+import sys
+import os
+import shutil
+import filecmp
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
+
+from _abathur.cluster import Clusterer
+
+
+class TestClustererFunctions(unittest.TestCase):
+
+    TMP_DIR = "/tmp/abathur_unittest"
+
+    def setUp(self):
+        tmp_dir = self.__class__.TMP_DIR
+
+        if os.path.exists(tmp_dir):
+            shutil.rmtree(tmp_dir)
+        os.makedirs(tmp_dir)
+
+    def tearDown(self):
+        tmp_dir = self.__class__.TMP_DIR
+        shutil.rmtree(tmp_dir)
+
+    def test_adhoc(self):
+
+        test_dir_name = os.path.dirname(__file__)
+        feat_array_fn = os.path.join(
+            test_dir_name, "data", "feature_array.csv")
+
+        feat_array = np.loadtxt(feat_array_fn, delimiter=",", skiprows=1)
+        print feat_array
+
+    def test_determine_k(self):
+        test_dir_name = os.path.dirname(__file__)
+        feat_array_fn = os.path.join(
+            test_dir_name, "data", "feature_array.csv")
+        feat_array = np.loadtxt(feat_array_fn, delimiter=",", skiprows=1)
+
+        clusterer = Clusterer("_", "_", [])
+        k = clusterer._determine_k(feat_array)
+        self.assertEqual(k, 2)
+
+    def test_perform_clustering(self):
+        test_dir_name = os.path.dirname(__file__)
+        feat_array_fn = os.path.join(
+            test_dir_name, "data", "feature_array.csv")
+
+        tmp_dir = self.__class__.TMP_DIR
+        output_fn = os.path.join(tmp_dir, "code.txt")
+
+        clusterer = Clusterer(feat_array_fn, output_fn, [])
+        clusterer.perform_clustering()
+
+        sample_output_filename = os.path.join(
+            test_dir_name, "data", "sample_cluster_code.txt")
+
+        # self.assertTrue(filecmp.cmp(output_fn, sample_output_filename))
+
+
+if __name__ == "__main__":
+    unittest.main()
