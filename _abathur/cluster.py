6231529bfee8c79870155f77b6aa02584048885a
Kexin Xie <ikexinxie@gmail.com>
2014-03-18 16:46:25 +1100

    imporve the cluster number estimation and added testing

--- _abathur/cluster.py
+++ _abathur/cluster.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.cluster.vq import vq, kmeans, whiten
 import pandas as pd
+from collections import defaultdict
 
 
 class Clusterer(object):
@@ -30,17 +31,19 @@ class Clusterer(object):
 
     def _determine_max_k(self, feat_array):
         """
-        The placeholder for better algorithm to determine k, for now we just
-        use k = sqrt(n/2) [1] as a naive starting point.
+        Determine the what is the maximum number of clusters to consider.
         [1] Kanti Mardia et al. (1979). Multivariate Analysis. Academic Press.
         """
 
         return int(round(np.sqrt(feat_array.shape[0] / 2.0)))
 
-    def _determine_k(self, feat_array, max_cluster=0):
+    def _determine_k(self, feat_array, max_cluster=0, multi_run=3):
         """
         Use the jump method to find ideal k by using an information theoretic
         approach.
+
+        In order to find a good jump, we run multiple times and the the lower
+        bound and use it to compute the best k to choose from.
 
         Ref:
         ----
@@ -49,35 +52,49 @@ class Clusterer(object):
         of clusters in a data set: An information theoretic approach".  Journal
         of the American Statistical Association 98 (January): 750-765.
         """
-        if max_cluster < 2:
-            max_cluster = self._determine_max_k(feat_array)
-        whitened = whiten(feat_array)
-        # first obtain the covariance matrix of the feature array
-        gamma = np.cov(whitened.T)
-        num_dim = whitened.shape[1]
-        jump = {}
-        distortions_dict = {0: 1}
-        power_fact = -num_dim / 2.0
-        # Run k mean for all possible number of clusters
-        for k in xrange(1, max_cluster + 1):
-            codebook, _ = kmeans(whitened, k, iter=self.iter)
-            code, _ = vq(whitened, codebook)
+        def _get_jump(feat_array, max_cluster):
+            if max_cluster < 2:
+                max_cluster = self._determine_max_k(feat_array)
+            whitened = whiten(feat_array)
+            # first obtain the covariance matrix of the feature array
+            gamma = np.cov(whitened.T)
+            num_dim = whitened.shape[1]
+            jump = {}
+            distortions_dict = {0: 1}
+            power_fact = -num_dim / 2.0
+            # Run k mean for all possible number of clusters
+            for k in xrange(1, max_cluster + 1):
+                codebook, _ = kmeans(whitened, k, iter=self.iter)
+                code, _ = vq(whitened, codebook)
 
-            clusters_dict = self._segment_to_clusters(whitened, code)
-            mahalanobis_dist_list = []
-            for cid, cvals in clusters_dict.iteritems():
-                centroid = codebook[cid]
-                cluster_mahalanobis_dist = map(
-                    lambda x: self._sq_mahalanobis(x, centroid, gamma),
-                    clusters_dict[cid].values)
-                mahalanobis_dist_list.extend(cluster_mahalanobis_dist)
-            this_distortion = np.mean(mahalanobis_dist_list) / num_dim
-            distortions_dict[k] = this_distortion ** power_fact
+                clusters_dict = self._segment_to_clusters(whitened, code)
+                mahalanobis_dist_list = []
+                for cid, cvals in clusters_dict.iteritems():
+                    centroid = codebook[cid]
+                    cluster_mahalanobis_dist = map(
+                        lambda x: self._sq_mahalanobis(x, centroid, gamma),
+                        clusters_dict[cid].values)
+                    mahalanobis_dist_list.extend(cluster_mahalanobis_dist)
+                this_distortion = np.mean(mahalanobis_dist_list) / num_dim
+                distortions_dict[k] = this_distortion ** power_fact
 
-        for k in xrange(1, max_cluster + 1):
-            jump[k] = distortions_dict[k] - distortions_dict[k - 1]
+            for k in xrange(1, max_cluster + 1):
+                jump[k] = distortions_dict[k] - distortions_dict[k - 1]
 
-        best_k = max(jump, key=jump.get)
+            return jump
+
+        jump_list = []
+        for i in xrange(multi_run):
+            jump_list.append(_get_jump(feat_array, max_cluster))
+
+        jump_combined = defaultdict(list)
+        for jump_dict in jump_list:
+            for idx, val in jump_dict.iteritems():
+                jump_combined[idx].append(val)
+        jump_final = dict([(i, min(v)) for i, v in jump_combined.iteritems()])
+
+        best_k = max(jump_final, key=jump_final.get)
+
         print "Chose {} as best number of clusters.".format(best_k)
 
         return best_k
